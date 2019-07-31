a070e6eaa9d674e06e0fc48e5c15883c0e3ababe
Bruce Brookshire <bruce.t.brookshire@gmail.com>
2019-07-31 18:29:36 -0500

    ios export environment

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/brucebrookshire/development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/brucebrookshire/Desktop/Skoller/skoller_flutter_mobile"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/brucebrookshire/development/flutter/bin/cache/artifacts/engine/ios-release"
+export "FLUTTER_BUILD_NAME=5.0.0"
